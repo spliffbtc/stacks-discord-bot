a4e693071d233ecf92ac00a59fc41ad18c88c3aa
aaronzeitlin <36874316+aaronzeitlin@users.noreply.github.com>
2022-04-08 07:39:06 -0700

    use collection.js

--- commands/collection/get-punk.js
+++ commands/collection/get-punk.js
@@ -1,42 +1,50 @@
 const { MessageEmbed } = require('discord.js');
+const getCollection = require('../../model/collection.js');
 
 // Look up NFT and provide links to collection
 module.exports = {
-	name: 'getpunk',
-	aliases: ['get', 'lookup', 'search', 'find'],
+	name: 'get',
+	aliases: ['lookup', 'search', 'find'],
 
 	execute(message, args) {
 		if (args[0] > 0 && args[0] < 9999) {
-			const punkID = args;
+			// Get Collection
+			const collection = getCollection();
+			const nftID = args;
+			// Create MessageEmbed
 			const embed = new MessageEmbed()
 				.setColor('#0099ff')
-				.setTitle('FreePunk #' + punkID)
-				.setURL('https://byzantion.xyz/marketplace/free-punks-v0')
+				.setTitle('FreePunk #' + nftID)
+				.setURL(`https://byzantion.xyz/marketplace/${collection.contractName}`)
 				.setImage(
-					`https://punks.fra1.cdn.digitaloceanspaces.com/assets/punks/punk${punkID}.png`,
+					`https://punks.fra1.cdn.digitaloceanspaces.com/assets/punks/punk${nftID}.png`,
 				)
 				.addFields(
 					{
 						name: 'FreePunks Website',
-						value: `View at freepunks.xyz: \n https://freepunks.xyz/details/${punkID}`,
+						value: `View at freepunks.xyz: \n https://freepunks.xyz/details/${nftID}`,
 					},
 
 					{
 						name: 'STXNFT',
-						value: `View at stxnft.com: \n https://stxnft.com/collections/free-punks/${punkID}`,
+						value: `View at stxnft.com: \n https://stxnft.com/collections/${collection.contractNameAlt}/${nftID}`,
 					},
 
 					{
 						name: 'BYZANTION',
-						value: `View at byzantion.xyz: \n https://byzantion.xyz/collection/free-punks-v0/${punkID}`,
+						value: `View at byzantion.xyz: \n https://byzantion.xyz/collection/${collection.contractName}/${nftID}`,
 					},
 				);
+				// Send MessageEmbed
 			message.channel.send({ embeds: [embed] });
 
 			return { embed };
 		}
 		else {
-			message.channel.send('Please enter a valid FreePunk ID');
+			// Get Collection
+			const collection = getCollection();
+			// Send Message
+			message.channel.send(`Please enter a valid ${collection.collectionName} ID`);
 		}
 	},
 };
