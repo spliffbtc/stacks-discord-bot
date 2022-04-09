4e3eabcf7132e05118bce507cd37808d3b0727c9
aaronzeitlin <36874316+aaronzeitlin@users.noreply.github.com>
2022-04-08 22:56:51 -0700

    misc copy

--- messages/onMention.js
+++ messages/onMention.js
@@ -3,7 +3,7 @@ const config = require('../botConfig.json');
 module.exports = {
 	async execute(message) {
 		return message.channel.send(
-			`What up, ${message.author}! My prefix is \`${config.prefix}\`, get help by \`${config.prefix}help\``,
+			`Hi, ${message.author}! My prefix is \`${config.prefix}\`, get help by \`${config.prefix}help\``,
 		);
 	},
 };
