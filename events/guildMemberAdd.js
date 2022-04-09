01b14494815d13610b63a7dbb64f3e11116aae80
aaronzeitlin <36874316+aaronzeitlin@users.noreply.github.com>
2022-04-09 10:03:55 -0700

    still buggy

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,13 +3,14 @@ const config = require('../botConfig.json');
 const welcomeChannel = config.channels.welcome;
 
 module.exports = (client) => {
-	client.on('guildMemberAdd', async member => {
+	client.on('guildMemberAdd', async (member) => {
 		const welcomeEmbed = new MessageEmbed();
 		welcomeEmbed.setTitle('Welcome');
 		welcomeEmbed.setDescription(`Welcome to the server, ${member}!`);
 		welcomeEmbed.setColor('#0099ff');
 		welcomeEmbed.setThumbnail(member.user.displayAvatarURL());
 		welcomeEmbed.setTimestamp();
-		welcomeChannel.send({ embeds: [welcomeEmbed] });
+		console.log(`${member.user.username} has joined the server.`);
+		member.guild.channels.get(welcomeChannel).send({ embeds: [welcomeEmbed] });
 	});
 };
