56d45250bbeb9c6eb0b380f41e63d3e5aa373c53
aaronzeitlin <36874316+aaronzeitlin@users.noreply.github.com>
2022-04-09 08:52:48 -0700

    welcome event

--- events/serverMemberAdd.js
+++ events/serverMemberAdd.js
@@ -2,26 +2,14 @@ const { MessageEmbed } = require('discord.js');
 const config = require('../botConfig.json');
 const welcomeChannel = config.channels.welcome;
 
-module.exports = {
-	name: 'welcome',
-	aliases: ['welcome'],
-	description: 'Welcome a new member to the server',
-	usage: 'welcome',
-	category: 'server',
-	permissions: 'SEND_MESSAGES',
-	clientPerms: 'SEND_MESSAGES',
-	args: false,
-	cooldown: 5,
-	async execute(client) {
-		await client.on('guildMemberAdd', member => {
-			member.roles.add(welcomeChannel);
-			const welcomeEmbed = new MessageEmbed();
-			welcomeEmbed.setTitle('Welcome');
-			welcomeEmbed.setDescription(`Welcome to the server, ${member}!`);
-			welcomeEmbed.setColor('#0099ff');
-			welcomeEmbed.setThumbnail(member.user.displayAvatarURL());
-			welcomeEmbed.setTimestamp();
-			welcomeChannel.send({ embeds: [welcomeEmbed] });
-		});
-	},
+module.exports = (client) => {
+	client.on('guildMemberAdd', async member => {
+		const welcomeEmbed = new MessageEmbed();
+		welcomeEmbed.setTitle('Welcome');
+		welcomeEmbed.setDescription(`Welcome to the server, ${member}!`);
+		welcomeEmbed.setColor('#0099ff');
+		welcomeEmbed.setThumbnail(member.user.displayAvatarURL());
+		welcomeEmbed.setTimestamp();
+		welcomeChannel.send({ embeds: [welcomeEmbed] });
+	});
 };
