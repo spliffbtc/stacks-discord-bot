49f376731ce7c2413df0343b0af7492f675df922
aaronzeitlin <36874316+aaronzeitlin@users.noreply.github.com>
2022-04-12 09:58:42 -0700

    fixed volume

--- commands/market/volume.js
+++ commands/market/volume.js
@@ -23,23 +23,14 @@ module.exports = {
 			.setTimestamp();
 
 		// Send Message
-		// Send Message
 		message.channel.send({ embeds: [embed] });
 		// Logging
-		
 		if (module.exports.args === false) {
 			console.log(`${message.author.tag} used the ${module.exports.name} command on ${message.guild.name}`);
 		}
 		else {
 			console.log(`${message.author.tag} used the ${module.exports.name} command on ${message.guild.name} with the following arguments: ${args}`);
 		}
-		// Logging
-		
-		if (module.exports.args === false) {
-			console.log(`${message.author.tag} used the ${module.exports.name} command on ${message.guild.name}`);
-		}
-		else {
-			console.log(`${message.author.tag} used the ${module.exports.name} command on ${message.guild.name} with the following arguments: ${args}`);
-		}
+
 	},
 };
