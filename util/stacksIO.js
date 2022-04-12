26c403883f0a1efeb675b7048cf869b514deb6f9
hz <13457698+hozzjss@users.noreply.github.com>
2022-04-12 16:15:38 +0200

    block sub

--- util/stacksIO.js
+++ util/stacksIO.js
@@ -4,9 +4,11 @@ const { io } = require('socket.io-client');
 const stacks = require('@stacks/blockchain-api-client');
 // const collection = require('../collectionConfig.json');
 
-
+/**
+ *
+ * @param {Client} client
+ */
 module.exports = async (client) => {
-
 	const socketUrl = 'https://stacks-node-api.mainnet.stacks.co/';
 	const socket = io(socketUrl, { transports: ['websocket'] });
 	const sc = new stacks.StacksApiSocketClient(socket);
@@ -16,13 +18,25 @@ module.exports = async (client) => {
 	const contractID = `${collection.contract.contractAddress}.${collection.contract.contractName}`;
 
 	// Subscriptions
-	sc.subscribeBlocks();
+	sc.subscribeBlocks((block) => {
+		client.guilds.cache.forEach((guild) => {
+			const channel = guild.channels.cache.find(
+				(c) => c.id === '961870023444553778',
+			);
+			if (!channel) return;
+			const embed = new MessageEmbed()
+				.setTitle('Block Received')
+				.setDescription(
+					`Block ${block.height} has been received by the Stacks network.`,
+				)
+				.setColor('#0099ff')
+				.setTimestamp();
+			channel.send(embed);
+		});
+	});
 	sc.subscribeMicroblocks();
 	sc.subscribeMempool();
 
 	// Handle Subscriptions
-	sc.handleSubscription(async (message) => {
-		console.log(message);
-		return message;
-	});
+	// const sub = sc.handleSubscription("block", true);
 };
