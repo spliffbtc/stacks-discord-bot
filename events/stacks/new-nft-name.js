ebdbe07fdd8f110fd8eb362222cd5e31845ef905
aaronzeitlin <36874316+aaronzeitlin@users.noreply.github.com>
2022-04-15 08:48:32 -0700

    playing with sockets

--- events/stacks/new-nft-name.js
+++ events/stacks/new-nft-name.js
@@ -6,10 +6,10 @@ const getTx = require('../../util/stacksAPI/transactions/get-transaction.js');
 const getBNS = require('../../util/stacksAPI/names/get-bns');
 
 // Build Contract ID
-const contractID = `${collection.contract.contractAddress}.${collection.contract.contractName}`;
+const contractID = 'SP158WX3ZNRDG4SR56TD1398NZK1945QTTKB8B60X.snns';
 
 module.exports = async (logger, client, sc) => {
-	logger.info('Listening for blocks...');
+	logger.info('Listening for new naming transactions...');
 	(await sc).subscribeBlocks(async (block) => {
 		block.txs.forEach(async (tx_id) => {
 			// Get Transaction Details
@@ -19,21 +19,25 @@ module.exports = async (logger, client, sc) => {
 			if (tx.data.tx_type === 'contract_call' && tx.data.contract_call.contract_id === contractID && tx.data.tx_status === 'success') {
 				console.log(tx.data);
 				const tx_id = tx.data.tx_id;
-				const BNS = await getBNS(tx.data.sender_address);
+				const BNS = getBNS(tx.data.sender_address);
 				const functionName = tx.data.contract_call.functionName;
 				const contract = tx.data.contract_call.contract_id;
-				const resultRepr = tx.data.tx_result.repr;
-				const nftID = resultRepr.replace('(ok u', '').replace(')', '');
+				const contractCall = tx.data.contract_call;
+				const namedCollection = contractCall.args[0];
+				const namedNFT = contractCall.args[1];
+				const nftName = contractCall.args[2];
 				const fee = tx.data.fee_rate / (10 ** 6);
 				const nonce = tx.data.nonce;
+				console.log(`${BNS} is renaming from ${namedCollection} for # ${namedNFT} with name ${nftName}`);
+				console.log(contractCall);
 
-				// MessageEmbed: New Mint Transaction
+				// MessageEmbed: New NFT Name Transaction
 				const embed = new MessageEmbed()
 					.setTitle('New Mint!')
 					.setColor('#0099ff')
 					.setURL(`https://explorer.stacks.co/transaction/${tx.data.tx_id}`)
 					.setDescription(
-						`Successful mint of ${tx.data.tx_id} by ${BNS}: ${nftID}`,
+						`${BNS} is renaming from ${namedCollection} for # ${namedNFT} with name ${nftName}`,
 					)
 					.setTimestamp();
 				// Send Embed
--- util/stacksIO.js
+++ util/stacksIO.js
@@ -5,10 +5,14 @@ module.exports = async (logger, client) => {
 	const newBlock = require('../events/stacks/new-block.js');
 	const newMicroblock = require('../events/stacks/new-microblock.js');
 	const newMintAttempt = require('../events/stacks/new-mint-attempt.js');
+	const newMint = require('../events/stacks/new-mint.js');
 	const newMarketTx = require('../events/stacks/new-market-tx.js');
+	const newNFTName = require('../events/stacks/new-nft-name.js');
 	await newBlock(logger, client, sc);
 	await newMicroblock(logger, client, sc);
 	await newMintAttempt(logger, client, sc);
+	await newMint(logger, client, sc);
 	await newMarketTx(logger, client, sc);
+	// await newNFTName(logger, client, sc);
 	logger.info('Socket connected to Stacks API');
 };
