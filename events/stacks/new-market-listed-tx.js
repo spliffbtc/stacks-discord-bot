f6e2871df500e5320b63cd34860f416b44b690f1
aaronzeitlin <36874316+aaronzeitlin@users.noreply.github.com>
2022-04-16 16:20:08 -0700

    console log testin

--- events/stacks/new-market-listed-tx.js
+++ events/stacks/new-market-listed-tx.js
@@ -10,44 +10,43 @@ const contractID = `${collection.contract.contractAddress}.${collection.contract
 
 module.exports = async (logger, client, sc) => {
 	logger.info('Listening for new listing txs...');
-	(await sc).subscribeBlocks(async (block) => {
-		block.txs.forEach(async (tx_id) => {
-			// Get Transaction Details
-			const tx = await getTx(tx_id);
-			// Collection Contract
-			if (tx.data.tx_type === 'contract_call' && tx.data.tx_status === 'success') {
-				if (tx.data.contract_call.contract_id.includes('market') === true) {
-					if (tx.data.contract_call.function_name.includes('list' || 'listed') === true) {
-						if (tx.data.contract_call.function_args[0].repr === contractID) {
-							const txID = tx.data.tx_id;
-							const BNS = await getBNS(tx.data.sender_address);
-							const resultRepr = tx.data.tx_result.repr;
-							const nftID = resultRepr.replace('(ok u', '').replace(')', '');
-							const fee = tx.data.fee_rate / (10 ** 6);
+	(await sc).subscribeMempool(async (mempool) => {
+		// Get Transaction Details
+		const tx = mempool;
+		// Collection Contract
+		if (tx.tx_type === 'contract_call') {
+			if (tx.contract_call.contract_id.includes('market') === true) {
+				console.log('Contract ID' + tx);
+				if (tx.contract_call.function_name.includes('list' || 'listed') === true) {
+					console.log('Function Name' + tx);
+					if (tx.contract_call.function_args[0].repr === contractID) {
+						console.log('OK' + tx);
+						const txID = tx.tx_id;
+						const BNS = await getBNS(tx.sender_address);
+						const fee = tx.fee_rate / (10 ** 6);
 
-							// MessageEmbed: New Market Tx Transaction
-							const embed = new MessageEmbed()
-								.setTitle('NFT Listed / Unlisted')
-								.setColor('#0099ff')
-								.setURL(`https://explorer.stacks.co/txid/${txID}`)
-								.setDescription(`${BNS} \nis attempting to list a new NFT.`)
-								.addFields(
-									{ name: 'Address', value: `${BNS}` },
-									{ name: 'nftID', value: `${nftID}` },
-									{ name: 'Fee', value: `${fee} STX` },
-								)
-								.setTimestamp();
+						// MessageEmbed: New Market Tx Transaction
+						const embed = new MessageEmbed()
+							.setTitle('NFT Listed / Unlisted')
+							.setColor('#0099ff')
+							.setURL(`https://explorer.stacks.co/txid/${txID}`)
+							.setDescription(`${BNS} \nis attempting to list a new NFT.`)
+							.addFields(
+								{ name: 'Address', value: `${BNS}` },
+								{ name: 'Fee', value: `${fee} STX` },
+							)
+							.setTimestamp();
 							// Send Embed
-							await client.channels.cache.get(channels.marketplace.listed).send({ embeds: [embed] });
-							console.log(embed);
-						}
-						else {return;}
+						await client.channels.cache.get(channels.marketplace.listed).send({ embeds: [embed] });
+						console.log(embed);
 					}
 					else {return;}
 				}
 				else {return;}
 			}
 			else {return;}
-		});
+		}
+		else {return;}
 	});
+
 };
--- events/stacks/new-market-sold-tx.js
+++ events/stacks/new-market-sold-tx.js
@@ -10,43 +10,42 @@ const contractID = `${collection.contract.contractAddress}.${collection.contract
 
 module.exports = async (logger, client, sc) => {
 	logger.info('Listening for new sales txs...');
-	(await sc).subscribeBlocks(async (block) => {
-		block.txs.forEach(async (tx_id) => {
-			// Get Transaction Details
-			const tx = await getTx(tx_id);
-			// Collection Contract
-			if (tx.data.tx_type === 'contract_call' && tx.data.tx_status === 'success') {
-				if (tx.data.contract_call.contract_id.includes('market') === true) {
-					if (tx.data.contract_call.function_name.includes('sold' || 'purchased' || 'bought') === true) {
-						if (tx.data.contract_call.function_args[0].repr === contractID) {
-							const txID = tx.data.tx_id;
-							const BNS = await getBNS(tx.data.sender_address);
-							const resultRepr = tx.data.tx_result.repr;
-							const nftID = resultRepr.replace('(ok u', '').replace(')', '');
-							const fee = tx.data.fee_rate / (10 ** 6);
+	(await sc).subscribeMempool(async (mempool) => {
+		// Get Transaction Details
+		const tx = mempool;
+		// Collection Contract
+		if (tx.tx_type === 'contract_call') {
+			if (tx.contract_call.contract_id.includes('market') === true) {
+				console.log('Contract ID' + tx);
+				if (tx.contract_call.function_name.includes('sold' || 'purchased' || 'bought') === true) {
+					console.log('Function Name' + tx);
+					if (tx.contract_call.function_args[0].repr === contractID) {
+						console.log('OK' + tx);
+						const txID = tx.tx_id;
+						const BNS = await getBNS(tx.sender_address);
+						const fee = tx.fee_rate / (10 ** 6);
 
-							// MessageEmbed: NFT Purchased / Sold
-							const embed = new MessageEmbed()
-								.setTitle('NFT Purchased / Sold')
-								.setColor('#0099ff')
-								.setURL(`https://explorer.stacks.co/txid/${txID}`)
-								.setDescription(`${BNS} \nhas purchased a new NFT.`)
-								.addFields(
-									{ name: 'Address', value: `${BNS}` },
-									{ name: 'nftID', value: `${nftID}` },
-									{ name: 'Fee', value: `${fee} STX` },
-								)
-								.setTimestamp();
+						// MessageEmbed: NFT Purchased / Sold
+						const embed = new MessageEmbed()
+							.setTitle('NFT Purchased / Sold')
+							.setColor('#0099ff')
+							.setURL(`https://explorer.stacks.co/txid/${txID}`)
+							.setDescription(`${BNS} \nhas purchased a new NFT.`)
+							.addFields(
+								{ name: 'Address', value: `${BNS}` },
+								{ name: 'Fee', value: `${fee} STX` },
+							)
+							.setTimestamp();
 							// Send Embed
-							await client.channels.cache.get(channels.marketplace.sold).send({ embeds: [embed] });
-						}
-						else {return;}
+						await client.channels.cache.get(channels.marketplace.sold).send({ embeds: [embed] });
 					}
 					else {return;}
 				}
 				else {return;}
 			}
 			else {return;}
-		});
+		}
+		else {return;}
+
 	});
 };
