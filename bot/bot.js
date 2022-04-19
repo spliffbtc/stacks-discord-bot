dd0ece1824361dfeae766fdf117b982a0bfb3e90
aaronzeitlin <36874316+aaronzeitlin@users.noreply.github.com>
2022-04-19 08:19:01 -0700

    dumb mistkae

--- bot/bot.js
+++ bot/bot.js
@@ -22,19 +22,13 @@ module.exports = () => {
 
 	// Logging
 	const logger = winston.createLogger({
-		level: 'info',
+		levels: winston.config.syslog.levels,
 		format: winston.format.combine(
 			winston.format.colorize(),
 			winston.format.json(),
 			winston.format.prettyPrint(),
 		),
-		defaultMeta: { service: 'user-service' },
 	});
-
-	//
-	// If we're not in production then log to the `console` with the format:
-	// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-	//
 	if (process.env.NODE_ENV !== 'production') {
 		logger.add(
 			new winston.transports.Console({
@@ -42,122 +36,124 @@ module.exports = () => {
 			}),
 		);
 	}
+	else {
 
-	// Create Client
-	const client = new Client({
-		intents: [
-			Intents.FLAGS.GUILDS,
-			Intents.FLAGS.GUILD_MESSAGES,
-			Intents.FLAGS.GUILD_MEMBERS,
-			Intents.FLAGS.GUILD_PRESENCES,
-		],
-	});
-
-	// Get Contract Details
-	// Currently not doing much for bot;
-	// eventually should be used instead of botConfig.json;
-	getContractDetails()
-		.then((results) => {
-			logger.info(`Contract Loaded: ${results.results.contract_id}`);
-			return results;
-		})
-		.catch((error) => {
-			console.error(error);
+		// Create Client
+		const client = new Client({
+			intents: [
+				Intents.FLAGS.GUILDS,
+				Intents.FLAGS.GUILD_MESSAGES,
+				Intents.FLAGS.GUILD_MEMBERS,
+				Intents.FLAGS.GUILD_PRESENCES,
+			],
 		});
 
-	// Event Handler
-	const eventFolders = fs.readdirSync('./bot/events');
-	for (const folder of eventFolders) {
-		const eventFiles = fs
-			.readdirSync(`./bot/events/${folder}`)
-			.filter((file) => file.endsWith('.js'));
-		for (const file of eventFiles) {
-			const event = require(`./events/${folder}/${file}`);
-			if (event.once) {
-				client.once(event.name, (...args) =>
-					event.execute(...args, client, logger),
-				);
-			}
-			else {
-				client.on(
-					event.name,
-					async (...args) => await event.execute(...args, client, logger),
-				);
-			}
-		}
-	}
-
-	// Define Command Collections
-	client.commands = new Collection();
-	client.slashCommands = new Collection();
-	client.buttonCommands = new Collection();
-	client.selectCommands = new Collection();
-	client.contextCommands = new Collection();
-	client.cooldowns = new Collection();
-	client.triggers = new Collection();
-
-	// Register Commands: Standard Commands
-	const commandFolders = fs.readdirSync('./bot/commands');
-	for (const folder of commandFolders) {
-		const commandFiles = fs
-			.readdirSync(`./bot/commands/${folder}`)
-			.filter((file) => file.endsWith('.js'));
-		for (const file of commandFiles) {
-			const command = require(`./commands/${folder}/${file}`);
-			client.commands.set(command.name, command);
-		}
-	}
-
-	// Register Commands: Slash Commands
-	const slashCommands = fs.readdirSync('./bot/interactions/slash');
-	for (const module of slashCommands) {
-		const commandFiles = fs
-			.readdirSync(`./bot/interactions/slash/${module}`)
-			.filter((file) => file.endsWith('.js'));
-
-		for (const commandFile of commandFiles) {
-			const command = require(`./interactions/slash/${module}/${commandFile}`);
-			client.slashCommands.set(command.data.name, command);
-		}
-	}
-
-	// Register Commands: Slash-Commands in Discord API
-	const rest = new REST({
-		version: '9',
-	}).setToken(token);
-	logger.info('Fetching commands...');
-	const commandJsonData = [
-		...Array.from(client.slashCommands.values()).map((c) => c.data.toJSON()),
-		...Array.from(client.contextCommands.values()).map((c) => c.data),
-	];
-	(async () => {
-		try {
-			logger.info('Refreshing application (/) commands');
-			await rest.put(Routes.applicationGuildCommands(clientID, guildID), {
-				body: commandJsonData,
+		// Get Contract Details
+		// Currently not doing much for bot;
+		// eventually should be used instead of botConfig.json;
+		getContractDetails()
+			.then((results) => {
+				logger.info(`Contract Loaded: ${results.results.contract_id}`);
+				return results;
+			})
+			.catch((error) => {
+				console.error(error);
 			});
-			logger.info('Successfully refreshed application (/) commands');
-		}
-		catch (error) {
-			console.error(error);
-		}
-	})();
 
-	// Registration of Message Based Chat Triggers
-	const triggerFolders = fs.readdirSync('./bot/triggers');
-	for (const folder of triggerFolders) {
-		const triggerFiles = fs
-			.readdirSync(`./bot/triggers/${folder}`)
-			.filter((file) => file.endsWith('.js'));
-		for (const file of triggerFiles) {
-			const trigger = require(`./triggers/${folder}/${file}`);
-			client.triggers.set(trigger.name, trigger);
+		// Event Handler
+		const eventFolders = fs.readdirSync('./bot/events');
+		for (const folder of eventFolders) {
+			const eventFiles = fs
+				.readdirSync(`./bot/events/${folder}`)
+				.filter((file) => file.endsWith('.js'));
+			for (const file of eventFiles) {
+				const event = require(`./events/${folder}/${file}`);
+				if (event.once) {
+					client.once(event.name, (...args) =>
+						event.execute(...args, client, logger),
+					);
+				}
+				else {
+					client.on(
+						event.name,
+						async (...args) => await event.execute(...args, client, logger),
+					);
+				}
+			}
 		}
+
+		// Define Command Collections
+		client.commands = new Collection();
+		client.slashCommands = new Collection();
+		client.buttonCommands = new Collection();
+		client.selectCommands = new Collection();
+		client.contextCommands = new Collection();
+		client.cooldowns = new Collection();
+		client.triggers = new Collection();
+
+		// Register Commands: Standard Commands
+		const commandFolders = fs.readdirSync('./bot/commands');
+		for (const folder of commandFolders) {
+			const commandFiles = fs
+				.readdirSync(`./bot/commands/${folder}`)
+				.filter((file) => file.endsWith('.js'));
+			for (const file of commandFiles) {
+				const command = require(`./commands/${folder}/${file}`);
+				client.commands.set(command.name, command);
+			}
+		}
+
+		// Register Commands: Slash Commands
+		const slashCommands = fs.readdirSync('./bot/interactions/slash');
+		for (const module of slashCommands) {
+			const commandFiles = fs
+				.readdirSync(`./bot/interactions/slash/${module}`)
+				.filter((file) => file.endsWith('.js'));
+
+			for (const commandFile of commandFiles) {
+				const command = require(`./interactions/slash/${module}/${commandFile}`);
+				client.slashCommands.set(command.data.name, command);
+			}
+		}
+
+		// Register Commands: Slash-Commands in Discord API
+		const rest = new REST({
+			version: '9',
+		}).setToken(token);
+		logger.info('Fetching commands...');
+		const commandJsonData = [
+			...Array.from(client.slashCommands.values()).map((c) => c.data.toJSON()),
+			...Array.from(client.contextCommands.values()).map((c) => c.data),
+		];
+		(async () => {
+			try {
+				logger.info('Refreshing application (/) commands');
+				await rest.put(Routes.applicationGuildCommands(clientID, guildID), {
+					body: commandJsonData,
+				});
+				logger.info('Successfully refreshed application (/) commands');
+			}
+			catch (error) {
+				console.error(error);
+			}
+		})();
+
+		// Registration of Message Based Chat Triggers
+		const triggerFolders = fs.readdirSync('./bot/triggers');
+		for (const folder of triggerFolders) {
+			const triggerFiles = fs
+				.readdirSync(`./bot/triggers/${folder}`)
+				.filter((file) => file.endsWith('.js'));
+			for (const file of triggerFiles) {
+				const trigger = require(`./triggers/${folder}/${file}`);
+				client.triggers.set(trigger.name, trigger);
+			}
+		}
+
+		// Listen for new blocks, microblocks, and transactions
+		stacksIO(logger, client);
+
+		// Log In Bot
+		client.login(token);
 	}
-
-	// Listen for new blocks, microblocks, and transactions
-	stacksIO(logger, client);
-
-	// Log In Bot
-	client.login(token);
 };
