bde4e6e0931f196fc755d54d4b3dccd0706f0466
aaronzeitlin <36874316+aaronzeitlin@users.noreply.github.com>
2022-04-18 23:02:26 -0700

    rm logging to file

--- bot/bot.js
+++ bot/bot.js
@@ -29,14 +29,6 @@ module.exports = () => {
 			winston.format.prettyPrint(),
 		),
 		defaultMeta: { service: 'user-service' },
-		transports: [
-			//
-			// - Write all logs with importance level of `error` or less to `error.log`
-			// - Write all logs with importance level of `info` or less to `combined.log`
-			//
-			new winston.transports.File({ filename: 'error.log', level: 'error' }),
-			new winston.transports.File({ filename: 'combined.log' }),
-		],
 	});
 
 	//
