4c466f9dc9f72e12d0f8b2c4fa2d63f09a4cbf9a
aaronzeitlin <36874316+aaronzeitlin@users.noreply.github.com>
2022-04-19 08:30:09 -0700

    wow

--- backend/models.js
+++ backend/models.js
@@ -1,6 +1,7 @@
 // Export Schemas
 exports.Bot = require('./model/botSchema');
-exports.Contract = require('./model/contractSchema');
 exports.Collection = require('./model/collectionSchema');
+exports.Contract = require('./model/contractSchema');
 exports.Marketplace = require('./model/marketplaceSchema');
+exports.Transaction = require('./model/transactionSchema');
 exports.User = require('./model/userSchema');
